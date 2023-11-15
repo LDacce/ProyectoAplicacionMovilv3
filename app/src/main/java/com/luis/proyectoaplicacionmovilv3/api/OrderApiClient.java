c2cd6393d3bc0604ab96c0b1a0bd5cb4f0069d33
Miguel Huanacchiri <miguelangel.huanacchiricastillo@emeal.nttdata.com>
2023-11-15 02:04:32 -0500

    OrderApiClient avance

--- app/src/main/java/com/luis/proyectoaplicacionmovilv3/api/OrderApiClient.java
+++ app/src/main/java/com/luis/proyectoaplicacionmovilv3/api/OrderApiClient.java
@@ -1,15 +1,42 @@
 package com.luis.proyectoaplicacionmovilv3.api;
 
-public class OrderApiClient {
-    private static final OrderApiClient ourInstance = new OrderApiClient();
+import com.luis.proyectoaplicacionmovilv3.models.CompanyModel;
+import com.luis.proyectoaplicacionmovilv3.models.EventModel;
 
-    private OrderApiClient() {
-        // Constructor privado para evitar la creación de instancias fuera de la clase
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public class OrderApiClient {
+    public interface OrderService {
+        @GET("masters/listEvents")
+        Call<List<EventModel>> getEventList();
+
+        @GET("masters/listCompanies")
+        Call<List<CompanyModel>> getCompanyList();
     }
 
+    private final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://transolyback-production.up.railway.app/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    private OrderApiClient.OrderService service;
+
+    private static final OrderApiClient ourInstance = new OrderApiClient();
     public static OrderApiClient getInstance() {
         return ourInstance;
     }
-
-    // Otros métodos y lógica de la clase
+    private OrderApiClient() {
+        // Constructor privado para evitar la creación de instancias fuera de la clase
+    }
+    public OrderApiClient.OrderService getService(){
+        if (service == null) {
+            service = retrofit.create(OrderApiClient.OrderService.class);
+        }
+        return service;
+    }
 }
